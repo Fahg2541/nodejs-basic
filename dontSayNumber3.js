542821f32c258b3a577087231cb330904e56a778
Fahg2541 <Fahg2541@gmail.com>
2019-09-21 14:38:18 +0700

    not 3

---
+++ dontSayNumber3.js
@@ -0,0 +1,11 @@
+function dontSayNumber(number) {
+    if (a[0] == "3" || a[1] == "3" || a[2] == "3") {
+        return "Eimogi"
+    } else {
+        return number;
+    }
+
+}
+
+let a = "10";
+console.log(dontSayNumber(a));
